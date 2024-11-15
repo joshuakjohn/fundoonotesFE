f693719dff4c093eb5aaaff511a7afa7a9484d90
Joshua K John <joshuakjohn7@gmail.com>
2024-11-15 22:24:48 +0530

    [joshua] add - added signup API call on signup component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpService } from 'src/app/services/http-service/http.service';
 
 @Component({
   selector: 'app-signup',
@@ -10,13 +12,13 @@ export class SignupComponent {
 
   registerForm!: FormGroup
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(private formBuilder: FormBuilder, private httpService: HttpService){}
 
   ngOnInit(){
 
     this.registerForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.minLength(3)]],
-      lastName: ['', Validators.required, Validators.minLength(1)],
+      fname: ['', [Validators.required]],
+      lname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
       confirm: ['', Validators.required]
@@ -25,5 +27,19 @@ export class SignupComponent {
   }
 
   get regFormControls() { return this.registerForm.controls; }
+
+  handleRegistration(){
+    if(this.registerForm.valid){
+      const { fname, lname, email, password } = this.registerForm.value
+      this.httpService.loginApiCall('/api/v1/users/signup', {fname, lname, email, password}).subscribe({
+        next: (res) => {
+          console.log(res)
+        },
+        error: (err) => {
+          console.log(err)
+        }
+      })
+    }
+  }
 
 }
