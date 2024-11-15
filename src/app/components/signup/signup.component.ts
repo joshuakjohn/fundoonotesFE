dd6b693601226d8052f8b06fd1d56531bfcead64
Joshua K John <joshuakjohn7@gmail.com>
2024-11-15 12:19:05 +0530

    [joshua] add - added validation part for signup

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  registerForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(){
+
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', Validators.required, Validators.minLength(1)],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirm: ['', Validators.required]
+    })
+
+  }
+
+  get regFormControls() { return this.registerForm.controls; }
 
 }
