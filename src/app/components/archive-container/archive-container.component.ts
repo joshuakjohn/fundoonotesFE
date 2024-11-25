70cea1f36b53ca0aa812c0b64cf6b0a7bc0b272f
Joshua K John <joshuakjohn7@gmail.com>
2024-11-25 18:49:11 +0530

    [joshua] add - added archive notes component

--- src/app/components/archive-container/archive-container.component.ts
+++ src/app/components/archive-container/archive-container.component.ts
@@ -1,4 +1,6 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpService } from 'src/app/services/http-service/http.service';
 
 @Component({
   selector: 'app-archive-container',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class ArchiveContainerComponent {
 
+  archiveList: any[] = []; 
+
+  constructor(public httpService: HttpService){}
+
+  ngOnInit() {
+
+    const header = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`);
+    this.httpService.getApiCall('/api/v1/notes', header).subscribe({
+      next: (res: any) => {
+        let list = res.data.map((note: {title: string, description:string, _id:string , isArchive:boolean, isTrash:boolean}) => ({title:note.title, description:note.description, _id:note._id, isArchive: note.isArchive}))
+        this.archiveList = list.filter((note: any) => note.isArchive === true)
+        console.log(this.archiveList)
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+    
+
+  }
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { MENU_ICON } from 'src/assets/svg-icons';
 
 @Component({
@@ -11,11 +12,15 @@ import { MENU_ICON } from 'src/assets/svg-icons';
 export class DashboardComponent {
   drawerState: boolean = false
   
-  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+  constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer, public router: Router) {
     iconRegistry.addSvgIconLiteral('menu-icon', sanitizer.bypassSecurityTrustHtml(MENU_ICON));
   }
 
   handleDrawerClick(){
     this.drawerState = !this.drawerState
+  }
+
+  navigateTo(route:string){
+    this.router.navigate(['/dashboard', route])
   }
 }
